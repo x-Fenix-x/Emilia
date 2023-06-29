dc09a3726a3c63b0e8a10387c3c261d72396e0e4
unknown <vallejosr@protonmail.com>
2023-06-29 05:49:19 -0300

    feat: Se agrega CSS al proyecto

--- app.js
+++ app.js
@@ -3,27 +3,15 @@ const app = express();
 const path = require('path');
 const PORT = 3030;
 
-/* RUTAS */
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views', 'home.html'));
-});
+/* configuraciones */
+app.use(express.static(path.resolve(__dirname,'public')));
 
-app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views', 'contact.html'));
-});
+/* rutas */
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
+app.get('/about',(req,res) => res.sendFile(path.resolve(__dirname, 'views','about.html')));
+app.get('/contact', (req,res) => res.sendFile(path.resolve(__dirname,'views','contact.html')));
+app.get('/music',(req,res) => res.sendFile(path.resolve(__dirname,'views','music.html')));
+app.get('/*',(req,res) => res.sendFile(path.resolve(__dirname,'views','404.html')));
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views', 'about.html'));
-});
 
-app.get('/music', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views', 'music.html'));
-});
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views', '404.html'));
-});
-
-app.listen(PORT, () =>
-  console.log(`Server corriendo en http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
